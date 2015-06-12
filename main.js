8eccbae4dd442f59b3498350f7a0a206474f3188
Yash Ranadive <yash.ranadive@gmail.com>
2015-06-12 08:54:31 -0700

    adding the teaser info

--- main.js
+++ main.js
@@ -8,18 +8,25 @@ $(document).ready(function() {
       }
   });
 
+  $(".teaser").click(function () {
+      $(".teaser-info").fadeToggle();
+  });
+
 });
 
 function getResults(token){
   var finalhtml="";
-  $.getJSON("http://127.0.0.1/select/?wt=json&indent=true&hl=true&hl.fl=description,command&hl.fl=description&hl.simple.pre=<strong>&hl.simple.post=</strong>&q=" + token, function(result){
+  $.getJSON("http://127.0.0.1/select/?wt=json&indent=true&hl=true&hl.fl=description,command:string&hl.fl=description&hl.simple.pre=<strong>&hl.simple.post=</strong>&q=" + token, function(result){
   for (var i = 0; i < result.response.docs.length; i++) {
-    var thisId = result.response.docs[i].id
+    var thisId = result.response.docs[i].id;
     var thisResult = ""
     + (result.highlighting[thisId].description || result.response.docs[i].description)
     + "<br><span class='command-snippet'>" + result.response.docs[i].command + "</span>"
     + "<br><br>";
     finalhtml += thisResult;
+  }
+  if(result.response.docs.length == 0) {
+    finalhtml = "<span class='no-result'>Can't find any snippets with that query</span>"
   }
   $('#rs').html(finalhtml);
   });
